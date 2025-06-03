9791ebe29a80ff1ebe74085e2a29a15507f975d2
KESAVAPRIYAN S <160122618+SKESAVAPRIYAN@users.noreply.github.com>
2025-06-03 15:04:50 +0530

    Time: 7 ms (57.61%), Space: 45.4 MB (51.87%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,31 +1,34 @@
 class Solution {
     public int minFallingPathSum(int[][] mat) {
-        int n=mat.length;
-        int m=mat[0].length;
-        if(n==1 && m==1) return mat[0][0];
-        int[][] dp=new int[n][m];
-        for(int i=0;i<n;i++){
-          dp[0][i]=mat[0][i];
-            }
-        for(int i=1;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(j==0){
-                dp[i][j]=mat[i][j]+Math.min(dp[i-1][j],dp[i-1][j+1]);
-            }
-                else if(j==(m-1)){
-                dp[i][j]=mat[i][j]+Math.min(dp[i-1][j],dp[i-1][j-1]);   
-                }
-                else{
-                dp[i][j]=mat[i][j]+Math.min(dp[i-1][j-1],(Math.min(dp[i-1][j],dp[i-1][j+1])));
-                }
+        int n = mat.length;
+        int m = mat[0].length;
+
+        if (n == 1 && m == 1) return mat[0][0];
+
+        int[][] dp = new int[n][m];
+
+        // Initialize the first row
+        for (int j = 0; j < m; j++) {
+            dp[0][j] = mat[0][j];
+        }
+
+        // Fill DP table
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int minAbove = dp[i - 1][j];
+                if (j > 0) minAbove = Math.min(minAbove, dp[i - 1][j - 1]);
+                if (j < m - 1) minAbove = Math.min(minAbove, dp[i - 1][j + 1]);
+
+                dp[i][j] = mat[i][j] + minAbove;
             }
         }
-        int ans=0;
-        for(int i=n-1;i<n;i++){
-            for(int j=1;j<m;j++){
-                ans=Math.min(dp[i][j],dp[i][j-1]);
-            }
+
+        // Find the minimum value in the last row
+        int ans = Integer.MAX_VALUE;
+        for (int j = 0; j < m; j++) {
+            ans = Math.min(ans, dp[n - 1][j]);
         }
+
         return ans;
     }
 }
